ef3f99d86a137b57d7d8e7c8df2efb91fb6285c4
Tony Ajeti <jetonajeti95@gmail.com>
2024-10-18 10:46:04 -0400

    Initial commit

---
+++ FetchChallenge/FetchChallenge/App/Entry/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Tony Ajeti on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
